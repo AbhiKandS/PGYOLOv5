b1251d8a85954a70fc715bf051ab87eb24e7b2f3
UltralyticsAssistant <web@ultralytics.com>
2026-03-30 04:28:43 +0000

    Auto-format by https://ultralytics.com/actions

--- classifier_test/original.py
+++ classifier_test/original.py
@@ -1,9 +1,6 @@
 from ultralytics import YOLO
 
 # Load your custom model using the new classification YAML
-model = YOLO('/Users/abhijithks/Downloads/yolov5s-cls.pt')
+model = YOLO("/Users/abhijithks/Downloads/yolov5s-cls.pt")
 
-metrics = model.val(
-   data='/Users/abhijithks/tmpZsh/final-dataset/pig-imageFolder/', 
-   split="test"
-)
+metrics = model.val(data="/Users/abhijithks/tmpZsh/final-dataset/pig-imageFolder/", split="test")
